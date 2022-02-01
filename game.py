83fe53dc48c49dda281e6fa82a3b7094c19cbf8e
gusta <gustavo_o_@hotmail.com>
2022-02-01 13:40:54 -0400

    Game de Matematica.

---
+++ game.py
@@ -0,0 +1,32 @@
+from models.calcular import Calcular
+
+
+def main() -> None:
+    pontos: int = 0
+    jogar(pontos)
+
+
+def jogar(pontos: int) -> None:
+    dificuldade: int = int(input("Infome o nível de dificuldade desejada [1, 2, 3, 4]: "))
+    calc: Calcular = Calcular(dificuldade)
+
+    print("Informe o resultado para a seguinte operação: ")
+    calc.mostrar_operacao()
+
+    resultado: int = int(input())
+
+    if calc.checar_resultado(resultado):
+        pontos += 1
+        print(f"Você tem: {pontos}")
+
+    continuar: int = int(input("Deseja continuar? [1 - sim, 0 - não] "))
+
+    if continuar:
+        jogar(pontos)
+    else:
+        print(f"Você finalizou com {pontos} pontos.")
+        print(f"bye!.")
+
+
+if __name__ == "__main__":
+    main()
